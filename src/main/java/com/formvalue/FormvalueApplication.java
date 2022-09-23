debc5da25c8494568fd5fb80cc0e78be896d032a
Niraj <111888750+nirajg18@users.noreply.github.com>
2022-09-23 17:27:02 +0530

    form_validation

---
+++ src/main/java/com/formvalue/FormvalueApplication.java
@@ -0,0 +1,13 @@
+package com.formvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormvalueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormvalueApplication.class, args);
+	}
+
+}
